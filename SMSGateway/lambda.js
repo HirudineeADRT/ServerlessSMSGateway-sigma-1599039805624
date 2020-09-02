2e6d0489a64a4820166c7b64998c8b3bda50e5fa
HirudineeADRT <hirudinee@adroitlogic.com>
2020-09-02 15:13:37 +0530

    furst commit

---
+++ SMSGateway/lambda.js
@@ -0,0 +1,37 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = async (event) => {
+
+	let receiver = event['receiver'];
+	let sender = event['sender'];
+	let message = event['message'];
+
+	let isPromotional = true;
+
+	console.log("Sending message", message, "to receiver", receiver);
+
+	try {
+		let data = await sns.publish({
+			Message: message,
+			PhoneNumber: receiver,
+			MessageAttributes: {
+				'AWS.SNS.SMS.SMSType': {
+					DataType: "String",
+					StringValue: "Promotional"
+				},
+				'AWS.SNS.SMS.SenderID': {
+					DataType: "String",
+					StringValue: sender
+				}
+			}
+		}).promise();
+
+		console.log("Sent message to", receiver);
+		return data;
+
+	} catch (err) {
+		console.log("Sending failed", err);
+		throw err;
+	}
+}
